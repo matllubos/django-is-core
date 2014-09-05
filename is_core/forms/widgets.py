9fcd728dc7bbd6c4de3b714a2fa7aca41c60ed32
Lubos Matl <matllubos@gmail.com>
2014-09-05 14:50:33 -0500

    v0.6.55 Remove directory from clearable file field

--- is_core/forms/widgets.py
+++ is_core/forms/widgets.py
@@ -1,11 +1,13 @@
 from __future__ import unicode_literals
 
+import os
+
 from itertools import chain
 
 from django import forms
 from django.utils.encoding import force_text
 from django.utils.safestring import mark_safe
-from django.utils.html import format_html, format_html_join
+from django.utils.html import format_html, format_html_join, conditional_escape
 from django.utils.translation import ugettext_lazy as _
 
 
@@ -86,7 +88,36 @@ class MultipleSelect(SelectMixin, forms.SelectMultiple):
     placeholder = _('Search...')
 
 
-class DragAndDropFileInput(forms.ClearableFileInput):
+class ClearableFileInput(forms.ClearableFileInput):
+
+    def render(self, name, value, attrs=None):
+
+        substitutions = {
+            'initial_text': self.initial_text,
+            'input_text': self.input_text,
+            'clear_template': '',
+            'clear_checkbox_label': self.clear_checkbox_label,
+        }
+        template = '%(input)s'
+        substitutions['input'] = super(forms.ClearableFileInput, self).render(name, value, attrs)
+
+        if value and hasattr(value, "url"):
+            template = self.template_with_initial
+            substitutions['initial'] = format_html(self.url_markup_template,
+                                                   value.url,
+                                                   os.path.basename(force_text(value)))
+            if not self.is_required:
+                checkbox_name = self.clear_checkbox_name(name)
+                checkbox_id = self.clear_checkbox_id(checkbox_name)
+                substitutions['clear_checkbox_name'] = conditional_escape(checkbox_name)
+                substitutions['clear_checkbox_id'] = conditional_escape(checkbox_id)
+                substitutions['clear'] = forms.CheckboxInput().render(checkbox_name, False, attrs={'id': checkbox_id})
+                substitutions['clear_template'] = self.template_with_clear % substitutions
+
+        return mark_safe(template % substitutions)
+
+
+class DragAndDropFileInput(ClearableFileInput):
 
     def render(self, name, value, attrs={}):
         id = attrs.get('id')
--- is_core/version.py
+++ is_core/version.py
@@ -1,4 +1,4 @@
-VERSION = (0, 6, 54)
+VERSION = (0, 6, 55)
 
 
 def get_version():
