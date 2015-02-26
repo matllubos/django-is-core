49208c56f34c29d5c5071d05240c1c67b44ee46f
Lubos Matl <matllubos@gmail.com>
2015-02-26 20:45:24 +0100

    v1.3.24 New version of germanium fixed dataprovider

--- is_core/version.py
+++ is_core/version.py
@@ -1,4 +1,4 @@
-VERSION = (1, 3, 23)
+VERSION = (1, 3, 24)
 
 def get_version():
     return '.'.join(map(str, VERSION))
--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
         'django>=1.6',
         'django-class-based-auth-views>=0.2',
         'django-piston==1.1.11',
-        'germanium>=0.2.2',
+        'germanium>=0.2.3',
         'django-block-snippets==0.0.11',
         'django-chamber>=0.0.7',
         'python-dateutil>=2.2',
@@ -48,7 +48,7 @@ setup(
         'https://github.com/matllubos/django-piston/tarball/1.1.11#egg=django-piston-1.1.11',
         'https://github.com/matllubos/django-chamber/tarball/0.0.7#egg=django-chamber-0.0.7',
         'https://github.com/matllubos/django-block-snippets/tarball/0.0.11#egg=django-block-snippets-0.0.11',
-        'https://github.com/LukasRychtecky/germanium/tarball/0.2.2#egg=germanium-0.2.2',
+        'https://github.com/LukasRychtecky/germanium/tarball/0.2.3#egg=germanium-0.2.3',
         'https://github.com/lukasrychtecky/django-project-info/tarball/0.2.4#egg=django-project-info-0.2.4'
     ],
     zip_safe=False
