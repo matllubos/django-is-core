9a928f4c7c734911b1925485d84e25246dd0581f
Lubos Matl <matllubos@gmail.com>
2015-07-17 16:48:54 +0200

    v1.4.2 Added new libraries

--- setup.py
+++ setup.py
@@ -31,9 +31,9 @@ setup(
         'django>=1.8',
         'django-class-based-auth-views>=0.3',
         'django-piston==1.2.1',
-        'germanium>=0.2.7',
+        'germanium>=1.0.0',
         'django-block-snippets==0.1.0',
-        'django-chamber>=0.1.1',
+        'django-chamber>=0.1.2',
         'python-dateutil>=2.2',
         'pytz',
         'django-apptemplates',
@@ -45,9 +45,9 @@ setup(
     ],
     dependency_links=[
         'https://github.com/matllubos/django-piston/tarball/1.2.1#egg=django-piston-1.2.1',
-        'https://github.com/matllubos/django-chamber/tarball/0.1.1#egg=django-chamber-0.1.1',
+        'https://github.com/matllubos/django-chamber/tarball/0.1.2#egg=django-chamber-0.1.2',
         'https://github.com/matllubos/django-block-snippets/tarball/0.1.0#egg=django-block-snippets-0.1.0',
-        'https://github.com/LukasRychtecky/germanium/tarball/0.2.7#egg=germanium-0.2.7',
+        'https://github.com/LukasRychtecky/germanium/tarball/1.0.0#egg=germanium-1.0.0',
         'https://github.com/lukasrychtecky/django-project-info/tarball/0.2.4#egg=django-project-info-0.2.4'
     ],
     zip_safe=False
