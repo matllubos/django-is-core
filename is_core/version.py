bb271ab4e62f2dc7f27ee9181da037c7f8d9f23f
Lubos Matl <matllubos@gmail.com>
2015-11-17 21:50:24 +0100

    v1.3.80 Removed germanium from packages

--- is_core/version.py
+++ is_core/version.py
@@ -1,4 +1,4 @@
-VERSION = (1, 3, 79)
+VERSION = (1, 3, 80)
 
 def get_version():
     return '.'.join(map(str, VERSION))
--- setup.py
+++ setup.py
@@ -31,7 +31,6 @@ setup(
         'django>=1.6',
         'django-class-based-auth-views>=0.2',
         'django-piston==1.1.21',
-        'germanium==0.2.10',
         'django-block-snippets==0.0.11',
         'django-chamber>=0.0.17',
         'python-dateutil>=2.2',
@@ -48,7 +47,6 @@ setup(
         'https://github.com/matllubos/django-piston/tarball/1.1.21#egg=django-piston-1.1.21',
         'https://github.com/matllubos/django-chamber/tarball/0.0.17#egg=django-chamber-0.0.17',
         'https://github.com/matllubos/django-block-snippets/tarball/0.0.11#egg=django-block-snippets-0.0.11',
-        'https://github.com/LukasRychtecky/germanium/tarball/0.2.10#egg=germanium-0.2.10',
         'https://github.com/lukasrychtecky/django-project-info/tarball/0.2.4#egg=django-project-info-0.2.4'
     ],
     zip_safe=False
