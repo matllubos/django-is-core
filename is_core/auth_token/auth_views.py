01b732b2b0cc1c28f9bfc2fb8965bc8f8885f522
Lubos Matl <matllubos@gmail.com>
2015-12-30 23:32:35 +0100

    v1.3.88 New version of django-security

--- is_core/auth_token/auth_views.py
+++ is_core/auth_token/auth_views.py
@@ -4,5 +4,4 @@ try:
     import security
     from .auth_security.views import TokenLoginView
 except ImportError as er:
-    print er
     from .default.views import TokenLoginView
--- is_core/version.py
+++ is_core/version.py
@@ -1,4 +1,4 @@
-VERSION = (1, 3, 87)
+VERSION = (1, 3, 88)
 
 
 def get_version():
