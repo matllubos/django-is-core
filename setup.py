596ad5313ae678f627572ec185749a671dae7384
Luboš Mátl <matllubos@gmail.com>
2019-06-10 20:16:12 +0200

    Used new snippets library

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
     install_requires=[
         'django>=1.10',
         'django-pyston~=2.9.5',
-        'django-block-snippets==2.0.0',
+        'django-block-snippets==2.0.1',
         'django-chamber~=0.4.6',
         'python-dateutil~=2.2',
         'pytz',
