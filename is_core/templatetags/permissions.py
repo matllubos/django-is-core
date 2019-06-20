874c848a7480be99f7ae43b3ed68b2b4640e2bcc
matllubos <matllubos@gmail.com>
2019-06-20 16:51:49 +0200

    Added new has permission to url tag

--- is_core/templatetags/permissions.py
+++ is_core/templatetags/permissions.py
@@ -1,6 +1,8 @@
-from django.template.base import Node, TemplateSyntaxError, NodeList
+from django.template.base import Node, TemplateSyntaxError, NodeList, kwarg_re
+from django.utils.encoding import force_text
 from django.utils.functional import SimpleLazyObject
 
+from is_core.utils import get_link_or_none
 from is_core.utils.compatibility import Library
 
 
@@ -69,3 +71,55 @@ def has_permission(parser, token):
     else:
         nodelist_false = NodeList()
     return PermissionNode(parser.compile_filter(bits[1]), vals, nodelist_true, nodelist_false)
+
+
+class PermissionURLNode(Node):
+
+    def __init__(self, pattern_name, kwargs, nodelist_true, nodelist_false):
+        self.pattern_name = pattern_name
+        self.nodelist_true = nodelist_true
+        self.nodelist_false = nodelist_false
+        self.kwargs = kwargs
+
+    def render(self, context):
+        pattern_name = self.pattern_name.resolve(context, True)
+        request = context.get('request')
+        kwargs = {
+            force_text(k, 'ascii'): v.resolve(context)
+            for k, v in self.kwargs.items()
+        }
+
+        link = get_link_or_none(pattern_name, request, view_kwargs=kwargs)
+        if link:
+            return self.nodelist_true.render(context.new({'url': link}))
+        else:
+            return self.nodelist_false.render(context)
+
+
+@register.tag
+def has_permission_to_url(parser, token):
+    bits = list(token.split_contents())
+    if len(bits) < 2:
+        raise TemplateSyntaxError('"has_permission_to_url" takes minimal one argument')
+
+    end_tag = 'end' + bits[0]
+
+    kwargs = {}
+    for bit in bits[2:]:
+        match = kwarg_re.match(bit)
+        if not match:
+            raise TemplateSyntaxError('Malformed arguments to has_permission_to_url tag')
+        name, value = match.groups()
+        if name:
+            kwargs[name] = parser.compile_filter(value)
+        else:
+            raise TemplateSyntaxError('Malformed arguments to has_permission_to_url tag')
+
+    nodelist_true = parser.parse(('else', end_tag))
+    token = parser.next_token()
+    if token.contents == 'else':
+        nodelist_false = parser.parse((end_tag,))
+        parser.delete_first_token()
+    else:
+        nodelist_false = NodeList()
+    return PermissionURLNode(parser.compile_filter(bits[1]), kwargs, nodelist_true, nodelist_false)
