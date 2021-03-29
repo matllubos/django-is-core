f6d9042ce0468966354a9c02ba0b296cc3b4c964
Luboš Mátl <matllubos@gmail.com>
2021-03-29 16:26:40 +0200

    Fixed inline view methods

--- is_core/generic_views/inlines/inline_table_views.py
+++ is_core/generic_views/inlines/inline_table_views.py
@@ -1,5 +1,6 @@
 from django.forms.models import _get_foreign_key
 
+from is_core.config import settings
 from is_core.generic_views.table_views import TableViewMixin
 from is_core.generic_views.inlines import RelatedInlineView
 
@@ -20,6 +21,16 @@ class InlineTableView(TableViewMixin, RelatedInlineView):
             self.related_core.get_ui_list_fields(self.request) if self.related_core and self.fields is None
             else self.fields
         )
+
+    def _get_export_fields(self):
+        return (
+            self.related_core.get_ui_export_fields(self.request) if self.related_core and self.export_fields is None
+            else self.export_fields
+        )
+
+    def _get_list_per_page(self):
+        list_per_page = self.related_core.get_list_per_page(self.request) if self.related_core else None
+        return list_per_page if list_per_page is not None else super()._get_list_per_page()
 
     def _get_api_url(self):
         return self.related_core.get_api_url(self.request)
